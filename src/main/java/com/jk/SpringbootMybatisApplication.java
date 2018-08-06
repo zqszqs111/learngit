907fe6de85a9cb315b728e550c65210a8610003f
Your Name <email@example.com>
2018-08-06 22:59:38 +0800

    测试项目one

--- src/main/java/com/jk/SpringbootMybatisApplication.java
+++ src/main/java/com/jk/SpringbootMybatisApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+//dsaddsadds
 @SpringBootApplication
 public class SpringbootMybatisApplication extends SpringBootServletInitializer{
 
